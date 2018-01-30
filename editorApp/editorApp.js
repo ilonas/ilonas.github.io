7b195ef2097380a62902dbd0ef38ec884703e941
ilonas <ilonas@wix.com>
2018-01-30 12:22:17 +0200

    Initial commit

--- editorApp/editorApp.js
+++ editorApp/editorApp.js
@@ -1,6 +1,38 @@
 'use strict';
 function getAppManifest() {
-  return {};
+  return { controllersStageData: {
+    controllerWithExportsAsObject: {
+        default: {
+            connections: {
+                buttonrole: {
+                    gfpp: {
+                        desktop: {
+                            mainAction1:{ actionId: 'EDIT', label: 'Edit This Btn' },
+                            mainAction2:{ actionId: 'MANAGE', label: 'Manage This Btn' },
+                            iconButtons: {
+                                design: {actionId: 'DESIGN_PANEL_OPENED' },
+                                layout:{actionId: 'LAYOUT_PANEL_OPENED'},
+                                animation: 'HIDE'
+                        },
+                            helpId: 'bc3c1b91-e9f4-441e-b89e-bb7801fe0b2c'
+                        },
+                        mobile: {
+                            mainAction1:{ actionId: 'MANAGE', label: 'Manage This Btn' },
+                            iconButtons: {
+                                layout: {actionId: 'MOBILE_LAYOUT_PANEL_OPENED'},
+                                animation: 'HIDE'
+                            },
+                            helpId: 'bc3c1b91-e9f4-441e-b89e-bb7801fe0b2c'
+                        }
+                    }
+                }
+            },
+            visibility: 'DEV'
+        }
+    }
+},
+};
+  
 }
 
 
